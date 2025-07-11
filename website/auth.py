39c3a93f9677e4ac1da529e2668adadb0d6849f0
Vince <vinceevangelista028@gmail.com>
2025-07-11 19:29:00 +0800

    finished user authentication

--- website/auth.py
+++ website/auth.py
@@ -2,16 +2,33 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth =  Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in successfully!", category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash("Wrong password", category='error')
+        else:
+            flash("Email is not registered in the system", category='error')
+    return render_template('login.html', user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>This is the logout page</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -38,9 +55,10 @@ def signup():
             db.session.commit()
 
             flash("Account succesfully created!", category='success')
+            login_user(user, remember=True)
             return redirect(url_for('views.home'))
 
         
-    return render_template('signup.html')
+    return render_template('signup.html', user=current_user)
 
 
